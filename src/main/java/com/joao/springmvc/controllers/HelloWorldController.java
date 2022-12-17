e35e21f437e39310b7c4195de0c924b89e7e1a0b
Joao Vitor <joaovitormoraveis@gmail.com>
2022-12-16 19:50:48 -0500

    Manipulating data from html form

---
+++ src/main/java/com/joao/springmvc/controllers/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.joao.springmvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/show-form")
+    public String showForm() {
+        return "hello-world-form";
+    }
+
+    @RequestMapping("/process-form")
+    public String processForm() {
+        return "hello-world";
+    }
+
+}
--- src/main/webapp/WEB-INF/views/jsp/homepage.jsp
+++ src/main/webapp/WEB-INF/views/jsp/homepage.jsp
@@ -1,5 +1,10 @@
 <html>
     <body>
         <h2>Spring MVC Demo - Home Page</h2>
+
+        <p>Table of Content</p>
+        <ol>
+          <li><a href="show-form" alt="Go to Form">Go to Form</a></li>
+        </ol>
     </body>
 </html>
