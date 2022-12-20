2e9cd1a7d2577d36e08b269414a7c7754e2342f8
Joao Vitor <joaovitormoraveis@gmail.com>
2022-12-19 19:54:53 -0500

    Spring mvc validation using regex

--- src/main/java/com/joao/springmvc/entity/Customer.java
+++ src/main/java/com/joao/springmvc/entity/Customer.java
@@ -3,6 +3,7 @@ package com.joao.springmvc.entity;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
@@ -17,6 +18,9 @@ public class Customer {
     @Min(value = 0, message = "must be greater than or equals to zero")
     @Max(value = 10, message = "must be less than or equals to ten")
     private Integer freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
 
     public String getFirstName() {
         return firstName;
@@ -40,5 +44,13 @@ public class Customer {
 
     public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
